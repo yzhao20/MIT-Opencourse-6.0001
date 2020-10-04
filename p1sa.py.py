f2845df6e207f990f852fff3b866d5ba8238c5df
Yunfeng <yzhao20@yahoo.com>
2020-10-04 15:02:47 +1100

    Add files via upload

---
+++ p1sa.py.py
@@ -0,0 +1,15 @@
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter % of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream House Price: "))
+portion_down_payment = total_cost * 0.25
+current_savings = 0
+r = 0.04
+n = 0
+while current_savings < portion_down_payment:
+    current_savings += current_savings * (r/12) + (annual_salary / 12) * portion_saved
+    n += 1
+print("Number of Months: ", n)
+
+
+
+
