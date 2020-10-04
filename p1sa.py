0f86d1fe47d7a06f81e36132f0a650fef3d215ef
Yunfeng <yzhao20@yahoo.com>
2020-10-04 15:04:54 +1100

    Update and rename p1sa.py.py to p1sa.py

--- p1sa.py
+++ p1sa.py
@@ -1,3 +1,9 @@
+"""
+This script is used to find how much months you need
+to afford to the downpayment of a house. Assuming you
+are save a certain percentage of your monthly salary to 
+saving account. 
+"""
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter % of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream House Price: "))
@@ -9,7 +15,3 @@ while current_savings < portion_down_payment:
     current_savings += current_savings * (r/12) + (annual_salary / 12) * portion_saved
     n += 1
 print("Number of Months: ", n)
-
-
-
-
